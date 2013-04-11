811701dcb62c4efe4616edad300d02e52132cb18
Dustin Zeisler <d@zyvo.us>
2013-04-10 20:33:24 -0700

    hightlight for current navigation

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ activate :blog do |blog|
   # blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  blog.layout = "blog_layout"
+  blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
@@ -21,7 +21,23 @@ end
 
 page "blog/feed.xml", :layout => false
 
-### 
+#blog blog_index
+#index
+# Methods defined in the helpers block are available in templates
+helpers do
+  def nav_active(current_page, page_link , title=nil)
+    title ||= page_link
+    if /\s/.match(current_page)
+      current_page = current_page.split(/\s/)[0]
+    end
+      if current_page == page_link then css_class = "active" else css_class = "" end
+      if page_link == "index"
+        page_link = "/"
+      end
+      content_tag(:li, link_to(title, page_link), :class => css_class)
+  end
+end
+###
 # Compass
 ###
 
@@ -39,13 +55,13 @@ page "blog/feed.xml", :layout => false
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -82,21 +98,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
